6253b763f9bfc2872b826da7a977c61beba14579
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-16 19:54:18 +0300

    add photogallery view

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alvin/Desktop/Moringa_Projects/Independent Projects/gallery/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
