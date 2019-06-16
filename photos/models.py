d86cde4d31411709324793c9bb6d0fe7bc901302
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-17 00:05:22 +0300

    setting up tests

--- photos/models.py
+++ photos/models.py
@@ -1,30 +1,38 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Editor(models.Model):
     '''
-    Editor model to show the user who is to upload the pictures
+    contains details of the user uploading the pictures
     '''
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
     phone_number = models.CharField(max_length = 10,blank =True)
 
+    def __str__(self):
+        return self.first_name
+
 class Location(models.Model):
     '''
-    Shows the location of the image
+    Contains the location of the image
     '''
     name = models.CharField(max_length =30)
 
 class Category(models.Model):
     '''
-    shows the category of the pictures
+    Contains the category of the pictures
     '''
     name = models.CharField(max_length =30, verbose_name = 'name')
 
+    class Meta:
+            verbose_name = "category"
+            verbose_name_plural = "categories"
+
 class Image(models.Model):
     '''
-    Shows the grid of images to be uploaded
+    Contains the grid of images to be uploaded
     '''
     image = models.ImageField(upload_to = 'images/', null=True)
     image_name = models.CharField(max_length =20)
@@ -32,3 +40,6 @@ class Image(models.Model):
     image_location = models.ForeignKey(Location, unique=False, blank=True)
     image_category = models.ForeignKey(Category, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from . models import Editor,Location,Category,Image
+import datetime as dt
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+    '''
+    Testing the Editor class
+    '''
+
+    #setup method
+    def setUp(self):
+        self.alvin= Editor(first_name = 'Alvin', last_name = 'Michoma', email = 'alvinmichoma@gnmail.com')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.alvin,Editor)) 
+        
