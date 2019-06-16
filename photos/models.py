75a1bd1f402e5b06b5f0755bc0a53ee5720b8f62
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-16 23:30:38 +0300

    create image  model

--- photos/models.py
+++ photos/models.py
@@ -21,3 +21,14 @@ class Category(models.Model):
     shows the category of the pictures
     '''
     name = models.CharField(max_length =30, verbose_name = 'name')
+
+class Image(models.Model):
+    '''
+    Shows the grid of images to be uploaded
+    '''
+    image = models.ImageField(upload_to = 'images/', null=True)
+    image_name = models.CharField(max_length =20)
+    image_description = models.TextField(max_length =50)
+    image_location = models.ForeignKey(Location, unique=False, blank=True)
+    image_category = models.ForeignKey(Category, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
