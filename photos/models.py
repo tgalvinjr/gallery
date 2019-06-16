7db95ef72d77b3e67b20a8b814e507eca53555eb
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-16 23:27:12 +0300

    create user model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Editor(models.Model):
+    '''
+    Editor model to show the user who is to upload the pictures
+    '''
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
