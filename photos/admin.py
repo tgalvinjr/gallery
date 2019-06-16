9b153ff9304ac7e97c16fda0521a28446bf8e843
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-17 00:50:39 +0300

    add admin functionalities

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Editor,Location,Category,Image
 # Register your models here.
+
+
+
+admin.site.register(Editor)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
