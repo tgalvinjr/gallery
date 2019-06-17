de7311f40ef12fd65756659ba47ccf5ee9ad06b3
tgalvinjr <tgalvin.jr@gmail.com>
2019-06-17 10:10:28 +0300

    add upload image functionaliy

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+urlpatterns = [
+    url('^$', views.welcome, name='welcome'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
